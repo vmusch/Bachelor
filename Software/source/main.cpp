842b764d09d7db1e1d76f594a78d0eebebf5a3bc
vmusch <vincent.musch@yahoo.com>
2022-09-01 11:32:46 +0200

    seqan submodule

--- Software/source/main.cpp
+++ Software/source/main.cpp
@@ -8,9 +8,21 @@
 #include "nfa_pointer.h"
 #include "utils.h"
 #include <seqan3/core/debug_stream.hpp>
+#include <seqan3/search/dream_index/interleaved_bloom_filter.hpp>
 
 int main(int argc, char *argv[])
 {
+  seqan3::interleaved_bloom_filter ibf{seqan3::bin_count{12u}, seqan3::bin_size{8192u}};
+  ibf.emplace(126, seqan3::bin_index{0u});
+  ibf.emplace(712, seqan3::bin_index{3u});
+  ibf.emplace(237, seqan3::bin_index{9u});
+
+  // Query the Interleaved Bloom Filter. Note that there may be false positive results!
+  // A `1` at position `i` indicates the (probable) presence of the query in bin `i`.
+  // Capture the result by reference to avoid copies.
+  auto agent = ibf.membership_agent();
+  auto & result = agent.bulk_contains(126);
+  seqan3::debug_stream << result << '\n'; // prints [0,0,0,1,0,0,0,0,0,0,0,0]
   seqan3::debug_stream << "Hello World!\n";
   if(argc > 5 || argc < 3)
   {
@@ -46,6 +58,7 @@ int main(int argc, char *argv[])
       {
         std::cout<<e<<" ";
       }
+      std::cout<<"\n";
       for(auto i : matrix)
       {
         for(auto j : i)
